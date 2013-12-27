3018245ce03328230445d11815fd7d40ef6768d4
sainaen <sainaen@gmail.com>
2013-12-27 12:07:58 -0500

    Add basic logging

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+var logger = require('koa-logger');
 var koa = require('koa');
+
 var app = koa();
+
+// logging
+app.use(logger());
 
 app.use(function* () {
   this.body = 'Hello, World!';
 });
 
 app.listen(3000);
+console.log('listening on port 3000');
