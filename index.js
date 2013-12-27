f8e34bca87eb1db650fa4f4f20f3f216cd2eb614
sainaen <sainaen@gmail.com>
2013-12-27 12:11:03 -0500

    Add basic posts listing and adding

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 var logger = require('koa-logger');
+var parse = require('co-body');
+var route = require('koa-route');
 var koa = require('koa');
 
 var app = koa();
 
+var posts = [];
+
 // logging
 app.use(logger());
 
-app.use(function* () {
-  this.body = 'Hello, World!';
-});
+// routing
+app.use(route.get('/', list));
+app.use(route.post('/posts', create));
+
+function* list() {
+  this.body = JSON.stringify(posts);
+}
+
+function* create() {
+  var post = yield parse(this);
+  var id = posts.push(post) - 1;
+  post.id = id;
+  post.created_at = new Date();
+
+  this.redirect('/');
+}
 
 app.listen(3000);
 console.log('listening on port 3000');
